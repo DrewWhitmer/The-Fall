1a7276afe4c6770f9198452653aae8fedf3b6632
Drew Whitmer <aswhitme@ucsc.edu>
2025-02-23 18:13:20 -0800

    initail commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
 'use strict'
 
 // name: Drew Whitmer
-// game: The Fall
-// hours: 20
-// creative tilt:
-// For the technical side of things, I am proud of how I managed to layer one scene on top of the other once the game ends in order
-// to make it so the player sees how they died.
-// For the artistic side of things, I am proud of the way the title screen has an animation that plays behind it, as well as the animation
-// that plays after you press 'play'.
+// hours: 1
 
 
 
@@ -15,7 +9,7 @@ let config = {
     type: Phaser.AUTO,
     width: 480,
     height: 640,
-    scene: [ Menu, Play, GameOver, ],
+    scene: [ Menu, Play,  ],
     physics: {
         default: 'arcade',
         arcade: {
--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -5,35 +5,7 @@ class Menu extends Phaser.Scene {
     
     preload() {
         //load images
-        this.load.image('faller', './assets/images/Player-Falling.png');
-        this.load.image('background', './assets/images/Background.png');
-        this.load.image('wall', './assets/images/Wall.png');
-        this.load.image('againButton', './assets/images/Again-Button.png');
-        this.load.image('menuButton', './assets/images/Menu-Button.png');
-        this.load.image('overScreen', './assets/images/Game-Over-Screen.png');
-        this.load.image('menuBackground', './assets/images/Menu-Background.png');
-        this.load.image('playButton', './assets/images/Play-Button.png');
-        this.load.image('title', './assets/images/Title.png');
-        this.load.image('ground', './assets/images/Menu-Ground-Spritesheet.png',);
-        this.load.image('leftArrow', './assets/images/Left-Arrow.png');
-        this.load.image('rightArrow', './assets/images/Right-Arrow.png');
-        this.load.image('fasterText', './assets/images/Faster-Text.png');
-        this.load.spritesheet('bat', './assets/images/Bat-Spritesheet.png', {
-            frameWidth: 64,
-            frameHeight: 64,
-        })
-        this.load.spritesheet('walkingPlayer', './assets/images/Player-Walking-Spritesheet.png', {
-            frameWidth: 64,
-            frameHeight: 64,
-        })
-
-        //load music/sfx (sfx made with jfxr, music gotten from opengameart.org)
-        this.load.audio('crack', './assets/sounds/Crack.wav');
-        this.load.audio('crash', './assets/sounds/Crash.wav');
-        this.load.audio('fall', './assets/sounds/Fall-Sound.wav');
-        this.load.audio('fallMusic', './assets/sounds/Fall-Music.ogg');
-        this.load.audio('walkMusic', './assets/sounds/Walk-Music.ogg');
-
+        this.load.image('mordecai', './assets/images/Mordecai.png');
     }
 
     create(num) {
@@ -44,70 +16,13 @@ class Menu extends Phaser.Scene {
 
         //settings
         game.settings = {
-            playerSpeed: 300,
-            speed: 5,
-            batSpeed: 200,
-            newObst: 300,
-            speedUp: 1500,
+
         }
-        //create music
-        this.music = this.sound.add('walkMusic');
-        this.music.loop = true;
-        this.music.play();
-        //create menu
-        this.background = this.add.tileSprite(0, -50, game.config.width, game.config.height, 'menuBackground').setOrigin(0,0);
-        this.title = this.add.sprite(game.config.width/2, game.config.height/5, 'title');
-        this.ground = this.add.sprite(0, game.config.height -50, 'ground').setOrigin(0,0);
-        this.player = this.add.sprite(game.config.width/2, game.config.height - 50, 'walkingPlayer', 0);
-        this.panic = false;
-        this.isFalling = false;
-        this.player.anims.create({
-            key: 'walk',
-            frameRate: 3,
-            repeat: -1,
-            frames: this.anims.generateFrameNumbers('walkingPlayer'),
-        })
-        this.player.anims.play('walk');
-        this.add.text(0,9*game.config.height/10, "music written and produced \nby Ove Melaa \n(Omsofware@hotmail.com) \nfrom opengameart.org\nall other assets by Drew Whitmer",
-            {
-                fontFamily: 'Georgia',
-                fontSize: 10,
-                color: '#000000',
-            }
-        )
 
-
-
-        this.playButton = this.add.sprite(game.config.width/2, game.config.height/2 + 185, 'playButton');
-        //start scene when play button is pressed
-        this.playButton.setInteractive({
-            useHandCursor: true,
-        })
-        this.playButton.on('pointerdown', () => {
-            this.music.stop()
-            this.playButton.destroy(),
-            this.player.anims.stop();
-            this.ground.x -= 480;
-            this.panic = true;
-            this.sound.play('crack')
-            this.clock = this.time.delayedCall(1000, () => {
-                this.ground.x -= 480,
-                this.player.destroy();
-                this.player = this.add.sprite(game.config.width/2, game.config.height - 50, 'faller');
-                this.player.angle = -15;
-                this.isFalling = true;
-                this.sound.play('fall');
-            }, null, this);
-            this.clock = this.time.delayedCall(2500, () => {this.scene.start("playScene", num);}, null, this);
-        });
+        this.scene.start('playScene');
     }
 
     update() {
-        if(this.isFalling) {
-            this.player.y++;
-        }
-        if(!this.panic) {
-            this.background.tilePositionX += 1;
-        }
+        
     }
 }
